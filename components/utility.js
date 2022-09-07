f1de33deb14f312d8c299e0b17781eae5a1726bd
Umesh Kumawat <68832856+Royanosh@users.noreply.github.com>
2022-09-07 23:12:33 +0530

    done

--- components/utility.js
+++ components/utility.js
@@ -6,24 +6,24 @@ export const closeNav = () => {
     document.getElementById("sidenav-wrap").removeAttribute("class");
 };
 
-export const tabs = (e, cityName) => {
+export const tabs = (elem, elemId) => {
     var i, tabcontent, tablinks, parentEle, tabContainer;
-    parentEle = e.parentNode;
-    tabContainer = e.parentNode.parentNode;
+    parentEle = elem.parentNode;
+    tabContainer = elem.parentNode.parentNode;
     tablinks = parentEle.children;
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    if (e.classList.contains("active")) {
-        e.classList.remove("active");
+    if (elem.classList.contains("active")) {
+        elem.classList.remove("active");
     } else {
-        e.classList.add("active");
+        elem.classList.add("active");
     }
     tabcontent = tabContainer.querySelectorAll(".tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(elemId).style.display = "block";
 };
 
 export const searchBox = () => {
