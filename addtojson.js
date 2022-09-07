26891cdea5bed877e01884323c1285ab2e5c27cf
unknown <you@example.com>
2022-09-07 12:41:02 +0530

    added all

--- addtojson.js
+++ addtojson.js
@@ -35,3 +35,54 @@ let addtojson=async()=>
 //     })
 //  }
 //  display();
+let addtoreanding=async()=>
+{
+    let obj=
+    {
+        image:document.getElementById("image1").value,
+        title:document.getElementById("title1").value,
+        cateogory:document.getElementById("cateogory1").value,
+        summary:document.getElementById("summary1").value,
+        date:document.getElementById("date1").value,
+        author:document.getElementById("author1").value,
+        onframe:document.getElementById("onframe1").value,
+        updateddate:document.getElementById("updateddate1").value,
+        body:document.getElementById("body1").value
+    }
+        let selected=document.getElementById("cateogory1").value;
+        let res1=await fetch("http://localhost:3000/HEAD-NEWS",
+        {
+                method:"POST",
+                body:JSON.stringify(obj),
+                headers:{
+                    "Content-type":"application/json"
+                  }
+        })
+        alert("Details added successfully")
+ }
+
+ let addtomostread=async()=>
+{
+    let obj=
+    {
+        image:document.getElementById("image2").value,
+        title:document.getElementById("title2").value,
+        cateogory:document.getElementById("cateogory2").value,
+        summary:document.getElementById("summary2").value,
+        date:document.getElementById("date2").value,
+        author:document.getElementById("author2").value,
+        onframe:document.getElementById("onframe2").value,
+        updateddate:document.getElementById("updateddate2").value,
+        body:document.getElementById("body2").value
+    }
+        let selected=document.getElementById("cateogory1").value;
+        let res1=await fetch("http://localhost:3000/MOST-READ",
+        {
+                method:"POST",
+                body:JSON.stringify(obj),
+                headers:{
+                    "Content-type":"application/json"
+                  }
+        })
+        alert("Details added successfully")
+ }
