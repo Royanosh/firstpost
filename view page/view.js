ad6aed8431184be96b843c117efdae3edc3bf155
sakti8093 <saktiprasad80932gmail.com>
2022-09-09 18:32:52 +0530

    added view page structure

--- view page/view.js
+++ view page/view.js
@@ -8,9 +8,11 @@ let viewpage=async()=>
     document.getElementById("fortitle").innerText=res2.title
     document.getElementById("forsummary").innerText=res2.summary
     document.getElementById("forauthor").innerText=res2.author
-    document.getElementById("fordate").innerText=res2.date
-
-    
+    document.getElementById("fordate").innerText=res2.date;
+    document.getElementById("forimage").src=res2.image;
+    document.getElementById("foronframe").innerText=res2.onframe;
+    document.getElementById("forbody").innerText=res2.body;
+    document.getElementById("forupdateddate").innerText=res2.updateddate;
 
 }
 viewpage();
